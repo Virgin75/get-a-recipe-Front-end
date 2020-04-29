0db73e02fabeb91f7e51bb21042c6e3e94a20b50
Virgin Bitton <virgin.bitton@gmail.com>
2020-04-29 23:11:18 +0200

    Update CSS

--- src/store.js
+++ src/store.js
@@ -3,15 +3,26 @@ import React from 'react'
 export const StoreContext = React.createContext(null)
 
 export default ({ children }) => {
-  const teamMembersNames = ['John', 'Mary', 'Jason', 'David']
 
-  const [sharing, setSharing] = React.useState([])
-  const [help, setHelp] = React.useState([])
-  const [pairing, setPairing] = React.useState(teamMembersNames)
   const [text, setText] = React.useState("")
+  const [formCount, setFormCount] = React.useState(3)
+
+
+  const [ingredient1, setIngredient1] = React.useState("")
+  const [ingredient2, setIngredient2] = React.useState("")
+  const [ingredient3, setIngredient3] = React.useState("")
+  const [ingredient4, setIngredient4] = React.useState("")
+  const [ingredient5, setIngredient5] = React.useState("")
+  const [ingredient6, setIngredient6] = React.useState("")
+  const [ingredient7, setIngredient7] = React.useState("")
+  const [ingredient8, setIngredient8] = React.useState("")
+
+
 
   const store = {
-    text, setText
+    text, setText, formCount, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient8, 
+    setIngredient1, setIngredient2, setIngredient3, setIngredient4, setIngredient5, setIngredient6, setIngredient7, setIngredient8, 
+    setFormCount,
   }
 
   return <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
