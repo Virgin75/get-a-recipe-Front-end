23cde118a9471b737acabe80576004a459cd5ad7
Virgin Bitton <virgin.bitton@gmail.com>
2020-05-01 17:23:13 +0200

    update routing system in index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './routes/Home';
-import {BrowserRouter as Router, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Recipe from './routes/Recipe'
 import StoreProvider from './store'
 
 
 ReactDOM.render(
   <StoreProvider>
-  <Router>
-    <>
-      <Route path='/recipe' component={Recipe} />
-      <Route path='/' component={Home} />
-    </>
-  </Router>
+
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/recipe' component={Recipe} />
+      </Switch>
+    </Router>
+  
   </StoreProvider>,
   document.getElementById('root')
 );
