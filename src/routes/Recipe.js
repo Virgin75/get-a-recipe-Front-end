3c842d881c7f2a93386e1133a9abd2e224b73fb8
Virgin Bitton <virgin.bitton@gmail.com>
2020-05-01 17:30:42 +0200

    implement design and logic of Recipe route

--- src/routes/Recipe.js
+++ src/routes/Recipe.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import '../App.css';
-import useFetch from '../useFetch';
+import Footer from '../components/Footer'
+import { Link } from 'react-router-dom'
 
 
-function Recipe() {
+function Recipe(props) {
   
+  const { data } = props.location.state
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0)
+  })
 
   return (
-    <div className="App">
-      <div>
-        <h3>Recipe</h3>
-      </div>
+    <>
+    <div className="topColorBar"> </div>
+
+    <div className="recipeContainer">
+        <Link to="/" style={{fontSize: '1.1rem', color: '#387D96', textDecoration: 'none'}}>> Go back to previous page</Link>
+        <br/>
+        <h1>‍🍳 {data.title}</h1>
+        <br/>
+        <ul>
+            {data.ingredients.map((value, index) => {
+              return <li key={index}>{value}</li>
+            })}
+        </ul>
+        <br/>
+        <b>Instructions:</b>
+        <br/>
+        <div className="grey">
+        <p className="instructions">{data.instructions}</p>
+        </div>
     </div>
+
+    <Footer />
+    </>
   );
 }
 
